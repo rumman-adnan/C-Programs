84fbc0ccbd5317906d5bc370b7617a0ee057780e
Rumman Adnan <109212385+rumman-adnan@users.noreply.github.com>
2022-10-18 16:29:21 +0500

    Update create-traverse-delete-node-detail.cpp

--- OOP and DS/create-traverse-delete-node-detail.cpp
+++ OOP and DS/create-traverse-delete-node-detail.cpp
@@ -1,122 +1,109 @@
 #include <iostream>
-#include <conio.h>
+#include <stdlib.h>
+//#include <conio.h>
 using namespace std;
 
 
-// dynamically create link list.
-
-class Node {      //class name should be start with capital letter
-public:
-
-int value;
-
-Node *next;
+class Node {
+	public: 
+	int value;
+	Node *next;
 };
 
 
 int main () {
-
-
-char choose;
-int num;
-int v;
-
-
-Node *head = new Node();     // Dynamic memory Allocation
-
-head = NULL;  // Null itsself is pointer to avoid garbage value
-
-cout << "Select any one choice: \n";
-
-while (1) {
-
-
-cout << "\n\nNew node, Select 1 " << endl;
-cout << "Traverse, Select 2" << endl;
-cout << "Delete last node, Select 3" << endl;
-cout << "Delete the last node with Deallocation, Select 4" << endl;
-cout << "Delete specific value from link list, Select 5" << endl;
-cout << "Choose any value: " << endl;
-
-
-cin >> choose;
-
-switch (choose) {
-
-	case '1': {
 	
-			Node *obj = new Node();
-			cout << "Enter any number: ";
-			cin >> num;
-			obj -> value = num;
-			obj ->next = head;
-			head = obj;
-			
-			break;
-	}
+	char choice;
+	int num;
+	int val;
+	Node *head = new Node();
+	head = NULL;
 	
 	
-	case '2': {
+	
+	while (1) {
 		
-			Node *temp;
-			temp = head;
-			cout << "The link list dynamic elements are as follows: "<< endl;
-			while (temp != NULL) {
-			cout << temp ->value << '\t';
-			temp = temp->next;
+		system("CLS");
+		cout << "#########################################\n";
+		cout << "Dynamic Allocation of Nodes in Link List\n";
+		cout << "#########################################\n\n";
+			
+		cout << "\n\nFor New node, Select 1 " << endl;
+		cout << "\nTo Traverse th nodes, Select 2" << endl;
+		cout << "\nTo Delete last node, Select 3" << endl;
+		cout << "\nDelete the last node and De-Allocate, Select 4" << endl;
+		cout << "\nDelete specific value from link list, Select 5" << endl;
+		cout << "\n\nYour Choice: ";
+		cin >>choice;
+		
+		switch (choice) {
+			
+			case '1': {
+				Node *obj = new Node();
+				cout << "\n\nEnter number: ";
+				cin >> num;
+				obj -> value = num;
+				obj -> next = head;
+				head = obj;
+				break;
 			}
-	
-	break;
-	}
-	case '3': {
-		
-	
-			head = head->next;
-			break;
-	}
-	case '4': {
-		
-			Node *temp;
-			temp = head;
-			head = head ->next;
-			delete(temp);
-			break;
-	}
-	case '5' : {
-		
-		Node *temp;
-		cout << "Enter the value which you want to delete from link list: ";
-		cin >> num;
-		Node *current = head;
-		while (current ->next!=NULL) {
 			
-			while (current->next->value == num) {
-				temp = current ->next;
-				current ->next = current ->next ->next;
+			case '2': {
+				
+				Node *temp; // No dynamic allocation, just for traversal
+				temp = head;
+				cout << "The link list dynamic elements are: ";
+				while (temp != NULL) {
+					
+					cout << temp -> value << '\t';
+					temp = temp -> next;
+				}
+				int addingNodes;
+				cout << "\nPress 1 for main menu: ";
+				cin >>addingNodes;	
+				if (addingNodes =1) {
+				break;
+				}		
+			}
+			
+			case '3': {
+				
+				head = head -> next;
+				break;
+			} 
+			case '4': {
+				
+				Node *temp;
+				temp = head;
+				head = head -> next;
+				delete (temp);
+				break;
+			}
+			
+			case '5': {
+				
+				Node *temp = head;
+				Node *current = head;
+				cout << "\n\nEnter number which you want to delete: ";
+				cin >> val;
+				
+				while (current -> next -> value == val ) {
+					
+					temp = current -> next;
+					current ->next = current -> next -> next;
+				}
 				delete(temp);
-			
-		}
-			
+				
+				break;
+			}
 		
-			
+		
+		
+		
 		}
 		
-		break;
+		
 	}
 	
-	
-	default:  {
-	
-		cout << " Select Again ";
-	}
-	}
-
-
-}
-
-
-
-
-
-return 0;
+	return 0;
 }
