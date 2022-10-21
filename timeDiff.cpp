dabc91aaab476ee6b61fa5f36d3cb61088a1a25f
Rumman Adnan <109212385+rumman-adnan@users.noreply.github.com>
2022-10-21 12:04:53 +0500

    Add files via upload

---
+++ timeDiff.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+	timeDiff(int h1, int m1, int h2, int m2) {
+		
+		
+		
+	}
+
+int main () {
+	
+	
+	int hour1, min1, hour2, min2;
+	cout << "Enter hour 1: ";
+	cin >> hour1;
+	
+	cout << "Enter minute 1: ";
+	cin >> min1;
+	
+	cout << "Enter hour 2: ";
+	cin >> hour2;
+	
+	cout << "Enter minute 2: ";
+	cin >> min2;
+	
+	timeDiff(hour1, min1, hour2, min2);
+	
+	
+	return 0;
+}
